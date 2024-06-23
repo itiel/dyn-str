78b6453a174b11c8e3930a012387dd5f03bcb594
Itiel Lopez <itiel@soyitiel.com>
2024-06-23 11:03:37 -0600

    Upgraded `test.c`
    Replaced `assert()` with custom test macros.

--- test.c
+++ test.c
@@ -3,9 +3,27 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <assert.h>
 
-#include "lib/dyn-str.h"
+#include "lib/dyn-str.h" 
+ 
+#define TEST(desc, test) \
+  ({ \
+    if (test) { \
+      printf("✅ %s\n", desc); \
+    } else { \
+      printf("❌ %s\n", desc); \
+      exit(EXIT_FAILURE); \
+    } \
+  }) 
+
+#define TEST_STEP(test) \
+  ({ \
+    int __result = (test); \
+    if (!__result) { \
+      fprintf(stderr, "Assestion failed: (%s)\n", #test); \
+    } \
+    __result; \
+  })
 
 int main () {
   DynStr_t * dStr1 = NULL;
@@ -18,130 +36,100 @@ int main () {
 
   printf("== DynStr Test ==\n\n");
 
-  printf("Dynamic string creation and destruction\n");
-  fflush(stdout);
-
-  assert(DynStr_init(&dStr1) == 0);
-  assert(dStr1->count == 0);
-  assert(DynStr_end(&dStr1) == 0);
-  assert(dStr1 == NULL);
-  
-  printf("Result: ✅\n\n");
-
-  printf("Dynamic string creation from C string\n");
-  fflush(stdout);
-
-  assert(DynStr_fromCStr(&dStr1, "Hello, World!") == 0);
-  assert(dStr1->count > 0);
-  assert(memcmp(dStr1->content, "Hello, World!", dStr1->count) == 0);
-  assert(DynStr_end(&dStr1) == 0);
-  
-  printf("Result: ✅\n\n");
-
-  printf("Dynamic string creation from another dynamic string\n");
-  fflush(stdout);
-
-  assert(DynStr_fromCStr(&dStr1, "Dynamica allocation!") == 0);
-  assert(DynStr_fromDynStr(&dStr2, dStr1) == 0);
-  assert(dStr1->count == dStr2->count);
-  assert(memcmp(dStr1->content, dStr2->content, dStr1->count) == 0);
-  assert(DynStr_end(&dStr1) == 0);
-  assert(DynStr_end(&dStr2) == 0);
-  
-  printf("Result: ✅\n\n");
-
-  printf("Dynamic string creation from another dynamic string with options\n");
-  fflush(stdout);
-
-  assert(DynStr_fromCStr(&dStr1, "Not a C string.") == 0);
-  assert(
-    DynStr_fromDynStrOpt(&dStr2, dStr1, (DynStrOptions_t) {
-      .offset = 8,
-      .count = 6,
-    }) == 0
+  TEST("Dynamic string creation and destruction",
+    TEST_STEP(DynStr_init(&dStr1) == 0) &&
+    TEST_STEP(dStr1->count == 0) &&
+    TEST_STEP(DynStr_end(&dStr1) == 0) &&
+    TEST_STEP(dStr1 == NULL)
   );
-  assert(memcmp(dStr2->content, "string", dStr2->count) == 0);
-  assert(DynStr_end(&dStr1) == 0);
-  assert(DynStr_end(&dStr2) == 0);
-  
-  printf("Result: ✅\n\n");
 
-  printf("Concatenation of C string to dynamic string\n");
-  fflush(stdout);
-
-  assert(DynStr_fromCStr(&dStr1, "Segmentation") == 0);
-  assert(DynStr_concatCStr(dStr1, " fault.") == 0);
-  assert(memcmp(dStr1->content, "Segmentation fault.", dStr1->count) == 0);
-  assert(DynStr_end(&dStr1) == 0);
-  
-  printf("Result: ✅\n\n");
-
-  printf("Concatenation of dynamic string to another dynamic string\n");
-  fflush(stdout);
-
-  assert(DynStr_fromCStr(&dStr1, "Heap") == 0);
-  assert(DynStr_fromCStr(&dStr2, " allocation.") == 0);
-  assert(DynStr_concatDynStr(dStr1, dStr2) == 0);
-  assert(memcmp(dStr1->content, "Heap allocation.", dStr1->count) == 0);
-  assert(DynStr_end(&dStr1) == 0);
-  assert(DynStr_end(&dStr2) == 0);
-  
-  printf("Result: ✅\n\n");
-
-  printf("Concatenation of dynamic string to another dynamic string with options\n");
-  fflush(stdout);
-
-  assert(DynStr_fromCStr(&dStr1, "String") == 0);
-  assert(DynStr_fromCStr(&dStr2, " concatenation.") == 0);
-  assert(
-    DynStr_concatDynStrOpt(dStr1, dStr2, (DynStrOptions_t) {
-      .offset = 0,
-      .count = 7,
-    }) == 0
+  TEST("Dynamic string creation from C string",
+    TEST_STEP(DynStr_fromCStr(&dStr1, "Hello, World!") == 0) &&
+    TEST_STEP(dStr1->count > 0) &&
+    TEST_STEP(memcmp(dStr1->content, "Hello, World!", dStr1->count) == 0) &&
+    TEST_STEP(DynStr_end(&dStr1) == 0)
   );
-  assert(memcmp(dStr1->content, "String concat", dStr1->count) == 0);
-  assert(DynStr_end(&dStr1) == 0);
-  assert(DynStr_end(&dStr2) == 0);
-  
-  printf("Result: ✅\n\n");
 
-  printf("Dynamic strings comparison\n");
-  fflush(stdout);
+  TEST("Dynamic string creation from another dynamic string",
+    TEST_STEP(DynStr_fromCStr(&dStr1, "Dynamica allocation!") == 0) &&
+    TEST_STEP(DynStr_fromDynStr(&dStr2, dStr1) == 0) &&
+    TEST_STEP(dStr1->count == dStr2->count) &&
+    TEST_STEP(memcmp(dStr1->content, dStr2->content, dStr1->count) == 0) &&
+    TEST_STEP(DynStr_end(&dStr1) == 0) &&
+    TEST_STEP(DynStr_end(&dStr2) == 0)
+  );
 
-  assert(DynStr_fromCStr(&dStr1, "Memory leak.") == 0);
-  assert(DynStr_fromCStr(&dStr2, "Memory leak.") == 0);
-  assert(DynStr_compare(dStr1, dStr2, &result) == 0);
-  assert(result);
-  assert(DynStr_end(&dStr1) == 0);
-  assert(DynStr_end(&dStr2) == 0);
-  
-  printf("Result: ✅\n\n");
+  TEST("Dynamic string creation from another dynamic string with options",
+    TEST_STEP(DynStr_fromCStr(&dStr1, "Not a C string.") == 0) &&
+    TEST_STEP(
+      DynStr_fromDynStrOpt(&dStr2, dStr1, (DynStrOptions_t) {
+        .offset = 8,
+        .count = 6,
+      }) == 0
+    ) &&
+    TEST_STEP(memcmp(dStr2->content, "string", dStr2->count) == 0) &&
+    TEST_STEP(DynStr_end(&dStr1) == 0) &&
+    TEST_STEP(DynStr_end(&dStr2) == 0)
+  );
 
-  printf("C string creation from dynamic string\n");
-  fflush(stdout);
+  TEST("Concatenation of C string to dynamic string",
+    TEST_STEP(DynStr_fromCStr(&dStr1, "Segmentation") == 0) &&
+    TEST_STEP(DynStr_concatCStr(dStr1, " fault.") == 0) &&
+    TEST_STEP(memcmp(dStr1->content, "Segmentation fault.", dStr1->count) == 0) &&
+    TEST_STEP(DynStr_end(&dStr1) == 0) 
+  );
 
-  assert(DynStr_fromCStr(&dStr1, "String copy.") == 0);
-  assert(DynStr_toCStr(dStr1, &cStr) == 0);
-  assert(cStr != NULL);
-  assert(strcmp(cStr, "String copy.") == 0);
-  assert(DynStr_end(&dStr1) == 0);
+  TEST("Concatenation of dynamic string to another dynamic string",
+    TEST_STEP(DynStr_fromCStr(&dStr1, "Heap") == 0) &&
+    TEST_STEP(DynStr_fromCStr(&dStr2, " allocation.") == 0) &&
+    TEST_STEP(DynStr_concatDynStr(dStr1, dStr2) == 0) &&
+    TEST_STEP(memcmp(dStr1->content, "Heap allocation.", dStr1->count) == 0) &&
+    TEST_STEP(DynStr_end(&dStr1) == 0) &&
+    TEST_STEP(DynStr_end(&dStr2) == 0) 
+  );
+
+  TEST("Concatenation of dynamic string to another dynamic string with options",
+    TEST_STEP(DynStr_fromCStr(&dStr1, "String") == 0) &&
+    TEST_STEP(DynStr_fromCStr(&dStr2, " concatenation.") == 0) &&
+    TEST_STEP(
+      DynStr_concatDynStrOpt(dStr1, dStr2, (DynStrOptions_t) {
+        .offset = 0,
+        .count = 7,
+      }) == 0
+    ) &&
+    TEST_STEP(memcmp(dStr1->content, "String concat", dStr1->count) == 0) &&
+    TEST_STEP(DynStr_end(&dStr1) == 0) &&
+    TEST_STEP(DynStr_end(&dStr2) == 0) 
+  );
+
+  TEST("Dynamic strings comparison",
+    TEST_STEP(DynStr_fromCStr(&dStr1, "Memory leak.") == 0) &&
+    TEST_STEP(DynStr_fromCStr(&dStr2, "Memory leak.") == 0) &&
+    TEST_STEP(DynStr_compare(dStr1, dStr2, &result) == 0) &&
+    TEST_STEP(result) &&
+    TEST_STEP(DynStr_end(&dStr1) == 0) &&
+    TEST_STEP(DynStr_end(&dStr2) == 0)
+  );
+
+  TEST("C string creation from dynamic string",
+    TEST_STEP(DynStr_fromCStr(&dStr1, "String copy.") == 0) &&
+    TEST_STEP(DynStr_toCStr(dStr1, &cStr) == 0) &&
+    TEST_STEP(cStr != NULL) &&
+    TEST_STEP(strcmp(cStr, "String copy.") == 0) &&
+    TEST_STEP(DynStr_end(&dStr1) == 0)
+  );
 
   free(cStr);
-  
-  printf("Result: ✅\n\n");
 
-  printf("Dynamic string memory reservation\n");
-  fflush(stdout);
+  TEST("Dynamic string memory reservation", 
+    TEST_STEP(DynStr_init(&dStr1) == 0) &&
+    TEST_STEP((result = (int) dStr1->capacity) == 0 || result > 0) &&
+    TEST_STEP(DynStr_reserve(dStr1, result + 1028) == 0) &&
+    TEST_STEP(dStr1->capacity > result) &&
+    TEST_STEP(DynStr_end(&dStr1) == 0)
+  );
 
-  assert(DynStr_init(&dStr1) == 0);
-  assert((result = (int) dStr1->capacity) == 0 || result > 0);
-  assert(DynStr_reserve(dStr1, result + 1028) == 0);
-  assert(dStr1->capacity > result);
-  assert(DynStr_end(&dStr1) == 0);
-  
-  printf("Result: ✅\n\n");
-
-  printf("All tests passed.\n");
+  printf("\nAll tests passed.\n");
 
   return 0;
 }
